d0b96f3097b7f2df7ac71eb0fea4a4800231b663
Nicholas <crazedglitch@yahoo.com>
2023-04-10 17:37:57 -0500

    Add Header and About components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(blogData) {
+    return (
+        <aside>
+            <img src={ blogData.image } alt="blog logo" />
+            <p>{blogData.about}</p>
+        </aside>
+    )
+}
+
+export default About;
